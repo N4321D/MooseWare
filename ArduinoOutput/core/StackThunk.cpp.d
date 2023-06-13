ededa95ff5a7bb0f37274994052d45ea5deb993a
N4321D <35295509+N4321D@users.noreply.github.com>
2023-06-13 11:50:42 -0700

    Delete ArduinoOutput directory

--- ArduinoOutput/core/StackThunk.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/dmitri/Documents/Work/Coding/App/0_0_Recording_Apps/rec_app/ArduinoOutput/core/StackThunk.cpp.o: \
- /home/dmitri/.arduino15/packages/rp2040/hardware/rp2040/3.2.0/cores/rp2040/StackThunk.cpp \
- /home/dmitri/.arduino15/packages/rp2040/hardware/rp2040/3.2.0/cores/rp2040/StackThunk.h
--- ArduinoOutput/libraries/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-/home/dmitri/Documents/Work/Coding/App/0_0_Recording_Apps/rec_app/ArduinoOutput/libraries/Adafruit_GFX_Library/glcdfont.c.o: \
- /home/dmitri/Arduino/libraries/Adafruit_GFX_Library/glcdfont.c
