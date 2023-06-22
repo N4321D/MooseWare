3174213fbd70d6cd5317bcfc36b34d5722342894
Dmitri <dmitrinyy@gmail.com>
2023-06-21 21:57:39 -0700

    added stimcontroller

--- rec_app/subs/gui/misc/Stimulation.py
+++ rec_app/subs/gui/misc/Stimulation.py
@@ -11,6 +11,12 @@ import numpy as np
 import math
 import random
 
+from kivy.clock import Clock
+from kivy.lang import Builder
+
+import time
+
+
 class Stimulation():
     protocol = []
 
@@ -95,60 +101,62 @@ class Stimulation():
         return (np.array(wave), np.array(wave_amp)), n_pulse
 
 
-class Stimulation1():
+class StimGenerator():
     def __init__(self) -> None:
         self.int_methods = {
-            'lin': self.linear_interpolate, 
-            'exp': self.exponential_interpolate, 
-            'log': self.logarithmic_interpolate, 
+            'lin': self.linear_interpolate,
+            'exp': self.exponential_interpolate,
+            'log': self.logarithmic_interpolate,
             'random': self.random_interpolate,
-            }  # avaiable interpolation methods
+        }  # avaiable interpolation methods
 
-    def create_stim(self,
-                    stim_Strt_T=1,
-                    stim_End_T=10,
-                    stim_method='lin',
-                    int_Strt_T=10,
-                    int_End_T=1,
-                    int_method='lin',
-                    amp_Strt=0,
-                    amp_End=100,
-                    amp_method='lin',
-                    n_pulse=10,
-                    **kwargs) -> tuple:
+    def _create_stim(self,
+                     stim_Strt_T=1,
+                     stim_End_T=10,
+                     stim_method='lin',
+                     int_Strt_T=10,
+                     int_End_T=1,
+                     int_method='lin',
+                     amp_Strt=0,
+                     amp_End=100,
+                     amp_method='lin',
+                     n_pulse=10,
+                     **kwargs) -> tuple:
         """
         Creates a callable that yields the next stimulation step.
 
         Args:
             stim_Strt_T (float): Start time of the stimulation (in seconds).
             stim_End_T (float): End time of the stimulation (in seconds).
-            stim_method (str): Method for generating the stimulation time steps ('lin', 'exp', 'log', or 'random').
+            stim_method (str): Method for generating the stimulation time steps 
+                               ('lin', 'exp', 'log', or 'random').
             int_Strt_T (float): Start time of the inter-stimulus interval (in seconds).
             int_End_T (float): End time of the inter-stimulus interval (in seconds).
-            int_method (str): Method for generating the inter-stimulus interval time steps ('lin', 'exp', 'log', or 'random').
+            int_method (str): Method for generating the inter-stimulus interval 
+                              time steps ('lin', 'exp', 'log', or 'random').
             amp_Strt (float): Start amplitude of the stimulation (between 0 and 1).
             amp_End (float): End amplitude of the stimulation (between 0 and 1).
-            amp_method (str): Method for generating the amplitude steps ('lin', 'exp', 'log', or 'random').
+            amp_method (str): Method for generating the amplitude steps 
+                              ('lin', 'exp', 'log', or 'random').
             n_pulse (int): Number of pulses in the stimulation sequence.
 
         Returns:
-            callable: A callable that yields the next stimulation step as a tuple (duration, amplitude).
+            tuple: (on time, off time, amplitude).
 
         """
 
-        if (err_meth:= {stim_method, int_method, amp_method}.difference(self.int_methods)):
+        if (err := {stim_method, int_method, amp_method}.difference(self.int_methods)):
             raise ValueError(
-                f"Invalid method(s): {', '.join(err_meth)}. "
+                f"Invalid method(s): {', '.join(err)}. "
                 "Available methods are 'lin', 'exp', 'log', and 'random'.")
 
-        on_time = self.int_methods[stim_method](stim_Strt_T, stim_End_T, n_pulse)
+        on_time = self.int_methods[stim_method](
+            stim_Strt_T, stim_End_T, n_pulse)
         off_time = self.int_methods[int_method](int_Strt_T, int_End_T, n_pulse)
         amp = self.int_methods[amp_method](amp_Strt, amp_End, n_pulse)
 
-
         for i in range(n_pulse):
             yield (next(on_time), next(off_time), next(amp))
-
 
     def linear_interpolate(self, start_value, end_value, num_steps):
         step_size = (end_value - start_value) / (num_steps - 1)
@@ -160,7 +168,7 @@ class Stimulation1():
         exponent = math.pow(ratio, 1 / (num_steps - 1))
         for i in range(num_steps):
             yield start_value * math.pow(exponent, i)
-    
+
     def logarithmic_interpolate(self, start_value, end_value, num_steps):
         ratio = math.log(end_value / start_value)
         increment = ratio / (num_steps - 1)
@@ -170,15 +178,112 @@ class Stimulation1():
     def random_interpolate(self, start_value, end_value, num_steps):
         for _ in range(num_steps):
             yield random.uniform(start_value, end_value)
-    
-    def calc_duration(self, start_stim, end_stim, stim_method, start_int, end_int, int_method, n_pulses):
-        on_time = self.int_methods[stim_method](start_stim, end_stim, n_pulses)
-        off_time = self.int_methods[int_method](start_int, end_int, n_pulses)
+
+    def calc_duration(self, 
+                      stim_Strt_T=1,
+                      stim_End_T=10,
+                      stim_method='lin',
+                      int_Strt_T=10,
+                      int_End_T=1,
+                      int_method='lin', 
+                      n_pulses=0, 
+                      **kwargs) -> float:
+        on_time = self.int_methods[stim_method](stim_Strt_T, stim_End_T, n_pulses)
+        off_time = self.int_methods[int_method](int_Strt_T, int_End_T, n_pulses)
         return sum(on_time) + sum(off_time)
-    
+
+
+class StimController(StimGenerator):
+    def __init__(self,) -> None:
+        super().__init__()
+        self.run = True
+        self.last_stim = (None, None, None)
+        self.stim_pars = dict(
+            stim_Strt_T=1,
+            stim_End_T=6,
+            stim_method='lin',
+            int_Strt_T=1,
+            int_End_T=0.1,
+            int_method='lin',
+            amp_Strt=0,
+            amp_End=100,
+            amp_method='lin',
+            n_pulse=10,
+        )
+        self.next_stim_event = Clock.schedule_once(lambda *x: x, 0)
+
+    def create_stim(self, **stim_pars):
+        self.stim_pars.update(stim_pars)
+        self.stim_generator = self._create_stim(**self.stim_pars)
+        self.last_stim = (None, None, None)
+        self.duration = self.calc_duration(**self.stim_pars)
+
+    def start_stim(self):
+        self.run = True
+        self.last_stim = (None, None, None)
+        self.do_next_stim()
+
+    def do_next_stim(self, *args):
+        if self.run and self.stim_generator:
+            try:
+                on_time, off_time, amp = self.last_stim = next(
+                    self.stim_generator)
+                next_stim_start = on_time + off_time
+                self.next_stim_event = Clock.schedule_once(self.do_next_stim, next_stim_start)
+                self.do_stim(on_time, amp)
+
+            except StopIteration:
+                # end of stim protocol
+                self.stop_stim()
+                return
+
+    def stop_stim(self):
+        self.next_stim_event.cancel()
+        self.run = False
+        self.do_stim(0, 0)
+
+    def reset_stim(self):
+        self.stop_stim()
+        self.create_stim()
+
+    def do_stim(self, duration, amp):
+        """
+        placeholder for function that is called when stim is created
+
+        Args:
+            duration (float): duration of stim in seconds
+            amp (float): amp of stim in %
+        """
+        print(f"stim for {duration} seconds, {amp}% amplitude")
+
 
 if __name__ == "__main__":
-    s = Stimulation()
-    s.protocol
+    from kivy.app import App
+    
+    class MyApp(App):
+        MACADDRESS = "00:00:00:00:00"
+        stim = StimController()
+        def build(self):
+            return Builder.load_string(
+"""
+BoxLayout:
+    Button:
+        text: "create stim"
+        on_release: app.stim.create_stim()
 
-    s1 = Stimulation1()
+    Button:
+        text: "start stim"
+        on_release: app.stim.start_stim()
+
+    Button:
+        text: "stop stim"
+        on_release: app.stim.stop_stim()
+    
+    Button:
+        text: "reset stim"
+        on_release: app.stim.reset_stim()
+
+""")
+
+    app = MyApp()
+    app.run()
