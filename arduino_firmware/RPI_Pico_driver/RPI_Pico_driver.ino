7ba1bca90c98641088fd6c0ac41ccc2bc2dc3ac8
Dmitri <dmitrinyy@gmail.com>
2023-08-22 14:22:41 -0700

    small edits

--- arduino_firmware/RPI_Pico_driver/RPI_Pico_driver.ino
+++ arduino_firmware/RPI_Pico_driver/RPI_Pico_driver.ino
@@ -48,7 +48,7 @@ struct textStr
   // struct with all text things
   const char *idle = "Standby...";
   const char *rec = "Recording...";
-  const char *defaultName = "RPI - Pico";
+  const char *defaultName = "RPiPico";
 };
 
 textStr texts;
@@ -325,8 +325,6 @@ void control(const char *key, JsonVariant value)
   }
   if (strcmp(key, "name") == 0)
   {
-    // strcpy(NAME, value.as<const char*>());
-    // feedback(NAME, 5, 20);
     setName(value.as<const char *>());
   }
 }
