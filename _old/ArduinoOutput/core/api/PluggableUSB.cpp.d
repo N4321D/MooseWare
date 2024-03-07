468da1332f41f4a2bbceb36894c3ed4840f1937a
Dmitri <35295509+N4321D@users.noreply.github.com>
2024-03-07 14:35:53 -0800

    Delete _old directory

--- _old/ArduinoOutput/core/api/PluggableUSB.cpp.d
+++
@@ -1,5 +0,0 @@
-/home/dmitri/Documents/Work/Coding/App/0_0_Recording_Apps/20231109/MooseWare/ArduinoOutput/core/api/PluggableUSB.cpp.o: \
- /home/dmitri/.arduino15/packages/rp2040/hardware/rp2040/3.3.2/cores/rp2040/api/PluggableUSB.cpp \
- /home/dmitri/.arduino15/packages/rp2040/hardware/rp2040/3.3.2/cores/rp2040/api/../../../ArduinoCore-API/api/PluggableUSB.cpp \
- /home/dmitri/.arduino15/packages/rp2040/hardware/rp2040/3.3.2/cores/rp2040/api/../../../ArduinoCore-API/api/USBAPI.h \
- /home/dmitri/.arduino15/packages/rp2040/hardware/rp2040/3.3.2/cores/rp2040/api/../../../ArduinoCore-API/api/PluggableUSB.h
