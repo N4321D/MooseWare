b8f2bb15142f1eb4270ea21055a1c74441f752f6
Dmitri <dmitrinyy@gmail.com>
2024-06-13 10:24:47 -0700

    updated version

--- rec_app/subs/gui/vars.py
+++ rec_app/subs/gui/vars.py
@@ -43,7 +43,7 @@ SPLASH_SCREEN_TIMEOUT = 2
 
 # SETTINGS (change able in settings)
 # TODO: replace other vars with these ones
-SETTINGS_VAR = {"Main": {"app_version": "2024.04.17",
+SETTINGS_VAR = {"Main": {"app_version": "2024.06.13",
                          'title': "MooseWare",
                          'app_logo': '└┘┘┘┘=|◶◶|=└└└└┘',
                          }
